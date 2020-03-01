461dbf11388452e4a94bffa5a50348aa8070b81a
Alex Bloom <lehanbloom@gmail.com>
2020-03-01 16:47:43 +0200

    Completed milestone 2.

---
+++ Milestone-2/js/Bloom-weekly.js
@@ -0,0 +1,9 @@
+let fibonacciX = 6;
+let fibonacciY = 8;
+
+function fibonacci() {
+  fibonacciX = document.getElementById("fibonacciX").innerHTML = fibonacciX;
+  fibonacciY = document.getElementById("fibonacciY").innerHTML = fibonacciY;
+}
+
+fibonacci();
