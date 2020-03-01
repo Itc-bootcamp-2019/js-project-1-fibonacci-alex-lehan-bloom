b0d7095141d5cfa1e5f3e1abd2001fc23a33c662
Alex Bloom <lehanbloom@gmail.com>
2020-03-01 20:47:10 +0200

    Final touches to milestone 4.

--- js/Bloom-weekly.js
+++ js/Bloom-weekly.js
@@ -1,26 +1,18 @@
-let fibonacciX = 0;
-let fibonacciY = 0;
-let fibList = [0, 1];
-let previousTwoNumbers = [];
-let userInput = 0;
-
+// Function for figuring out fibonacci
 function fibonacci(x) {
-  fibonacciX = x;
-  fibonacciY = fibList[-1];
+  let fibList = [0, 1];
+  let fibonacciY = fibList[-1];
   for (i = 1; i < x; i++) {
-    previousTwoNumbers = fibList.slice(-2);
+    let previousTwoNumbers = fibList.slice(-2);
     fibonacciY = previousTwoNumbers[0] + previousTwoNumbers[1];
     fibList.push(fibonacciY);
   }
-  document.getElementById("x").innerHTML = fibonacciX;
-  document.getElementById("y").innerHTML = fibonacciY;
+  document.getElementById("y").innerText = fibonacciY;
 }
 
+// Detects button click
 let button = document.getElementById("button");
 button.addEventListener("click", () => {
-  userInput = document.getElementById("inputField").value;
-  fibonacci(userInput);
-  fibList = [0, 1];
+  fibonacciX = document.getElementById("inputField").value;
+  fibonacci(fibonacciX);
 });
-
-// fibonacci(17);
