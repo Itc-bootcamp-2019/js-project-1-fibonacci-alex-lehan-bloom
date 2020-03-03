9536fe7dc703fdf69edcbbbe25a2bb1fd839d447
Alex Bloom <lehanbloom@gmail.com>
2020-03-03 20:07:45 +0200

    Milestone 5 with Fetch instead of AJAX.

--- js/Bloom-weekly.js
+++ js/Bloom-weekly.js
@@ -1,17 +1,31 @@
-
-//Milestone 5. Send X to remote server and get back Y. I used AJAX.
+// Milestone 5 with Fetch. Send X to remote server and get back Y.
 function buttonClicked() {
   fibonacciX = document.getElementById("inputField").value;
-  let xhr = new XMLHttpRequest();
-  let url = "http://localhost:5050/fibonacci/" + fibonacciX;
-  xhr.open("GET", url);
-  xhr.responseType = "text";
-  xhr.onload = function() {
-    // alert(`Loaded: ${xhr.status} ${xhr.response}`);
-    let all = JSON.parse(xhr.response);
-    let y = all.result;
-    console.log("The y is: " + y);
-    document.getElementById("y").innerText = y;
-  };
-  xhr.send();
+  fetch("http://localhost:5050/fibonacci/" + fibonacciX)
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+      let y = data.result;
+      document.getElementById("y").innerText = y;
+    });
 }
+
+//Milestone 5 with AJAX. Send X to remote server and get back Y.
+
+// function buttonClicked() {
+//   fibonacciX = document.getElementById("inputField").value;
+//   let xhr = new XMLHttpRequest();
+//   let url = "http://localhost:5050/fibonacci/" + fibonacciX;
+//   xhr.open("GET", url);
+//   xhr.responseType = "text";
+//   xhr.onload = function() {
+//     // alert(`Loaded: ${xhr.status} ${xhr.response}`);
+//     let all = JSON.parse(xhr.response);
+//     let y = all.result;
+//     console.log("The y is: " + y);
+//     document.getElementById("y").innerText = y;
+//   };
+//   xhr.send();
+// }
