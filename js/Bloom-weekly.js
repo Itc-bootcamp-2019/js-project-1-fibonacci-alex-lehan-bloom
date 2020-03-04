a5ee8e1233c243469b40b0edbda285f128da648a
Alex Bloom <lehanbloom@gmail.com>
2020-03-04 13:58:47 +0200

    Milestone 6 - loading spinner is working.

--- js/Bloom-weekly.js
+++ js/Bloom-weekly.js
@@ -1,31 +1,41 @@
-// Milestone 5 with Fetch. Send X to remote server and get back Y.
-function buttonClicked() {
+// Detects when button is clicked. When button is clicked, we send iniate getYFromServer.
+let button = document.getElementById("button");
+button.addEventListener("click", getYFromServer);
+
+// Milestone 5 with Fetch. Send X to remote server and get back Y. We display Y to the user.
+function getYFromServer() {
+  document.getElementById("y").innerText = "";
+  showSpinner();
   fibonacciX = document.getElementById("inputField").value;
-  fetch("http://localhost:5050/fibonacci/" + fibonacciX)
-    .then(response => {
-      return response.json();
-    })
-    .then(data => {
-      console.log(data);
-      let y = data.result;
-      document.getElementById("y").innerText = y;
-    });
+  if (fibonacciX < 0 || fibonacciX > 50) {
+    let alert = document.getElementById("alert");
+    alert.innerText = "This is a test.";
+    hideSpinner();
+  } else {
+    fetch("http://localhost:5050/fibonacci/" + fibonacciX)
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        console.log(data);
+        let y = data.result;
+        hideSpinner();
+        document.getElementById("y").innerText = y;
+      });
+  }
 }
 
-//Milestone 5 with AJAX. Send X to remote server and get back Y.
+// Show the spinner during API request
+function showSpinner() {
+  const spinner = document.getElementById("spinner");
+  spinner.className = "show";
+  setTimeout(() => {
+    spinner.className = spinner.className.replace("show", "");
+  }, 8000);
+}
 
-// function buttonClicked() {
-//   fibonacciX = document.getElementById("inputField").value;
-//   let xhr = new XMLHttpRequest();
-//   let url = "http://localhost:5050/fibonacci/" + fibonacciX;
-//   xhr.open("GET", url);
-//   xhr.responseType = "text";
-//   xhr.onload = function() {
-//     // alert(`Loaded: ${xhr.status} ${xhr.response}`);
-//     let all = JSON.parse(xhr.response);
-//     let y = all.result;
-//     console.log("The y is: " + y);
-//     document.getElementById("y").innerText = y;
-//   };
-//   xhr.send();
-// }
+// Hide spinner once API request has completed
+function hideSpinner() {
+  const spinner = document.getElementById("spinner");
+  spinner.className = "";
+}
