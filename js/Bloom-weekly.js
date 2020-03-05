2ae8c6096fc10e6d0b1985ca53b26d12e4007bad
Alex Bloom <lehanbloom@gmail.com>
2020-03-05 18:56:16 +0200

    Updated JS so results re-load on every request.

--- js/Bloom-weekly.js
+++ js/Bloom-weekly.js
@@ -1,6 +1,24 @@
-// Displays previous results from server.
+// //Milestone 7.1
+// function previousResults() {
+//   console.log("hello")
+//   showSpinnerTwo();
+//   let response = await fetch("http://localhost:5050/getFibonacciResults");
+//   let data = await response.json();
+//   for (let i = 0; i < data.results.length; i++) {
+//     let results = document.getElementById("results-list");
+//     let date = new Date(data.results[i].createdDate).toUTCString();
+//     let newLi = document.createElement("li");
+//     newLi.innerHTML = `The fibonacci Of <span class="bold">${data.results[i].number}</span> is <span class="bold">${data.results[i].result}</span>. Calculated at: ${date}`;
+//     results.appendChild(newLi);
+//   }
+//   hideSpinnerTwo();
+// }
+
+previousResults();
 function previousResults() {
   showSpinnerTwo();
+  let results = document.getElementById("results-list");
+  results.innerHTML = "";
   fetch("http://localhost:5050/getFibonacciResults")
     .then(response => {
       return response.json();
@@ -17,12 +35,11 @@ function previousResults() {
     });
 }
 
-// On button click, we initate checkNumFromUser().
+// On button click, initate validateNumFromUser().
 let button = document.getElementById("button");
-button.addEventListener("click", checkNumFromUser);
+button.addEventListener("click", validateNumFromUser);
 
-// Function to validate number inputted by user
-function checkNumFromUser() {
+function validateNumFromUser() {
   // Remove items left over from any previous runs of the function (e.g. alerts, the last y number, etc.)
   hideAlert();
   document.getElementById("y").innerText = "";
@@ -43,21 +60,16 @@ function checkNumFromUser() {
     getYFromServer(fibonacciX);
   }
 
-  // Send X to remote server and get back Y. We display Y to the user OR an error if they entered something wrong.
   function getYFromServer(fibonacciX) {
     fetch("http://localhost:5050/fibonacci/" + fibonacciX)
       .then(response => {
-        // This starts handling for the '42' error., which returns a 400.
-        // If 400, we must return the data as Text to display error message sent by server. Otherwise, we return it as an Object.
-        if (response.status === 400) {
+        if (response.status === 400 || response.status === 500) {
           return response.text();
         } else {
           return response.json();
         }
       })
       .then(data => {
-        // If the data is text, we print the text to the user as an error.
-        // If data is an object, everything has gone succesfully and we print the Y number to the user.
         if (typeof data === "object") {
           hideSpinner();
           document.getElementById("y").innerText = data.result;
@@ -71,38 +83,32 @@ function checkNumFromUser() {
   }
 }
 
-// Show the spinner during API request
 function showSpinner() {
-  const spinner = document.getElementById("spinner");
-  spinner.className = "show";
+  let spinner = document.getElementById("spinner");
+  spinner.style.display = "inline-block";
   setTimeout(() => {
     spinner.className = spinner.className.replace("show", "");
   }, 8000);
 }
 
-// Hide spinner
 function hideSpinner() {
-  const spinner = document.getElementById("spinner");
-  spinner.className = "";
+  let spinner = document.getElementById("spinner");
+  spinner.style.display = "none";
 }
 
-//Show spinner 2 while previous results are loading
 function showSpinnerTwo() {
-  const spinner = document.getElementById("spinner-results");
-  console.log("@@");
-  spinner.className = "show";
+  let spinner = document.getElementById("spinner-results");
+  spinner.style.display = "inline-block";
   setTimeout(() => {
     spinner.className = spinner.className.replace("show", "");
   }, 8000);
 }
 
-// Hide spinner 2
 function hideSpinnerTwo() {
-  const spinner = document.getElementById("spinner-results");
-  spinner.className = "";
+  let spinner = document.getElementById("spinner-results");
+  spinner.style.display = "none";
 }
 
-// Show alert when user enters invalid X value into input field
 function showAlert() {
   const alert = document.getElementById("alert");
   alert.className = "alert alert-danger show";
@@ -110,7 +116,6 @@ function showAlert() {
   inputField.className = "form-control red";
 }
 
-// Hide alert
 function hideAlert() {
   const alert = document.getElementById("alert");
   alert.className = "";
